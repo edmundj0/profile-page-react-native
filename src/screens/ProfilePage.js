55041c7a184a7ebabe44aaca243fb5f048d225a7
Edmund Ju <edmundju@berkeley.edu>
2023-07-12 18:29:35 -0700

    profile pic up and install react native paper

---
+++ src/screens/ProfilePage.js
@@ -0,0 +1,29 @@
+// import { SafeAreaView, View } from "react-native"
+import  { Avatar }  from "react-native-paper"
+import { SafeAreaView, View } from "react-native"
+
+const ProfilePage = () => {
+    return (
+        // <SafeAreaView>
+        //     <View>
+        //         <View>
+        //             <Avatar>
+
+        //             </Avatar>
+        //         </View>
+        //     </View>
+
+        // </SafeAreaView>
+        // <Text>test</Text>
+        <SafeAreaView>
+            <View>
+                <View>
+                    <Avatar.Image />
+                </View>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+
+export default ProfilePage;
